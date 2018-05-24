f84296d29b7a3835a32b739e0d659ff62f38aa9a
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-05-24 13:30:44 -0300

    Add fonts to precompile list

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,5 +15,11 @@ Rails.application.configure do
     leaflet/dist/images/marker-shadow.png
     leaflet/dist/images/layers.png
     leaflet/dist/images/layers-2x.png
+    fontawesome/fonts/FontAwesome.otf
+    fontawesome/fonts/fontawesome-webfont.eot
+    fontawesome/fonts/fontawesome-webfont.svg
+    fontawesome/fonts/fontawesome-webfont.ttf
+    fontawesome/fonts/fontawesome-webfont.woff
+    fontawesome/fonts/fontawesome-webfont.woff2
   )
 end
