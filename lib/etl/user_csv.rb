18f6bafe3207a6c163b3e38da54ac36b5e88a979
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-08-14 20:29:36 -0300

    Default values for profile attributes on user_csv

--- lib/etl/user_csv.rb
+++ lib/etl/user_csv.rb
@@ -6,7 +6,7 @@ require 'etl/user_csv/transformations/find_or_create_layer'
 module Etl
   module UserCsv
     class Job
-      def import!(file, profile_attributes)
+      def import!(file, profile_attributes = {})
 
         job = Kiba.parse do
           source CsvSource, file
