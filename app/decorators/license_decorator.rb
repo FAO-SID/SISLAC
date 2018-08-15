64f8dfb6c7c410a459842ee50b40687ba2c5d093
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-08-15 10:53:15 -0300

    Show license information on Profile

--- app/decorators/license_decorator.rb
+++ app/decorators/license_decorator.rb
@@ -2,4 +2,8 @@ class LicenseDecorator < ApplicationDecorator
   def full_name
     "#{source.name} (#{source.acronym})"
   end
+
+  def link
+    h.link_to full_name, url, target: '_blank'
+  end
 end
