386b4ba86d5839c2467d9a8772c1a81271916a44
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-08-16 14:42:07 -0300

    Show index/search total count (closes #10)

--- app/decorators/paginador_decorator.rb
+++ app/decorators/paginador_decorator.rb
@@ -1,4 +1,4 @@
 # encoding: utf-8
 class PaginadorDecorator < Draper::CollectionDecorator
-  delegate :current_page, :total_pages, :limit_value, :total_count, :pagina
+  delegate :current_page, :total_pages, :limit_value, :total_count, :pagina, :entry_name, :offset_value
 end
