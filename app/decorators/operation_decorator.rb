58e20cb77c78d87c17ff7289a9f529c7ea52fb93
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-09-28 07:12:55 -0300

    More information on Processes

--- app/decorators/operation_decorator.rb
+++ app/decorators/operation_decorator.rb
@@ -6,4 +6,8 @@ class OperationDecorator < ApplicationDecorator
   def download_link
     h.link_to object.results.original_filename, h.download_operation_path(object) if object.finished?
   end
+
+  def start_time
+    h.l object.created_at, format: :short
+  end
 end
